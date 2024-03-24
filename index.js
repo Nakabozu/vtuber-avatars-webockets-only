f27a3314b2522dd5cb01fe5d0f6ceaaf769d10b4
Naka Freitag <spamnaka@gmail.com>
2024-03-24 15:49:05 -0400

    Adding cors shenanigans back in

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 
-// import cors from "cors";
+import cors from "cors";
 // import { readFileSync } from "fs";
 
 const PORT = process.env.PORT || 3674;
 const app = express();
-//app.use(cors());
+app.use(cors());
 
 const server = http.createServer(app);
 
@@ -30,25 +30,26 @@ app.get('/', (req, res) => {
 });
 
 const io = new Server(server
-    //     , {
-    //     handlePreflightRequest: (req, res) => {
-    //         const headers = {
-    //             "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    //             "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-    //             "Access-Control-Allow-Credentials": true
-    //         };
-    //         res.writeHead(200, headers);
-    //         res.end();
-    //     },
-    //     cors: {
-    //         origin: "*",
-    //         methods: ["GET", "POST"]
-    //     }
-    // }
+        , {
+        handlePreflightRequest: (req, res) => {
+            const headers = {
+                "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+                "Access-Control-Allow-Credentials": true
+            };
+            res.writeHead(200, headers);
+            res.end();
+        },
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }
+    }
 );
 
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
+    socket.emit("welcome", usersEmotions);
 
     socket.on("client_requests_emotions", (callback) =>{
         console.log(`Client wants to know all of the emotions`);
